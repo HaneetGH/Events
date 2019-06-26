516887ffb2a8676f4629e4fb90af831db85e31e6
haneetgh <HOTh@n!!t52p92>
2019-06-26 20:35:36 +0530

    date filter include

--- app/src/main/java/technorapper/com/event/menu/MainActivity.java
+++ app/src/main/java/technorapper/com/event/menu/MainActivity.java
@@ -7,9 +7,19 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import technorapper.com.event.R;
 
 import technorapper.com.event.RecyclerViewClickListener;
@@ -18,6 +28,7 @@ import technorapper.com.event.menu.adapter.ListAdapter;
 import technorapper.com.event.menu.base.SystemBase;
 import technorapper.com.event.menu.detail.DetailScreen;
 import technorapper.com.event.menu.viewmodel.ViewModel;
+import technorapper.com.event.networking.model.Result;
 import technorapper.com.event.networking.model.UnisysModel;
 
 
@@ -41,6 +52,7 @@ public class MainActivity extends SystemBase implements RecyclerViewClickListene
             @Override
             public void onChanged(@Nullable UnisysModel unisysModel) {
                 mUnisysModelResponse = unisysModel;
+                //   activateFilter();
                 fillData(unisysModel);
             }
         });
@@ -49,9 +61,84 @@ public class MainActivity extends SystemBase implements RecyclerViewClickListene
 
     }
 
+    Date date = null;
+    Date date2 = null;
+
+    private void activateFilter() {
+
+        binding.filter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                if (selectedItem.equals("Date")) {
+                    // do your stuff
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+
+                    Observable.just(mUnisysModelResponse.getResults()).flatMap(Observable::fromIterable)
+                            .toSortedList((complaintRowModel, complaintRowModel2) -> {
+                                try {
+                                    date = format.parse(complaintRowModel.getStart());
+                                    date2 = format.parse(complaintRowModel2.getStart());
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+
+                                // comparing dates is very much dependent on your implementation
+                                if (date.before(date2)) {
+                                    return -1;
+                                } else if (date.equals(date2)) {
+                                    return 0;
+                                } else {
+                                    return 1;
+                                }
+                            })
+
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(this::handleResult, this::handleError);
+                }
+            } // to close the onItemSelected
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+            private void handleError(Throwable throwable) {
+                Log.d("Issue", throwable.toString());
+            }
+
+            private void handleResult(List<Result> results) {
+                ListAdapter mAdapter = new ListAdapter(results, MainActivity.this);
+                binding.setAdapter(mAdapter);
+            }
+        });
+    }
+
     @Override
     public void bindView() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+        /*binding.filter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Observable.just(mUnisysModelResponse.getResults())
+                        .flatMap(Observable::fromIterable)
+                        .toSortedList()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(this::handleSensors, this::handleError);
+            }
+
+            private void handleError(Throwable throwable) {
+            }
+
+            private void handleSensors(List<Result> results) {
+                ListAdapter mAdapter = new ListAdapter(results, MainActivity.this);
+                binding.setAdapter(mAdapter);
+            }
+
+        });*/
 
 
     }
@@ -61,6 +148,7 @@ public class MainActivity extends SystemBase implements RecyclerViewClickListene
 
         ListAdapter mAdapter = new ListAdapter(response.getResults(), MainActivity.this);
         binding.setAdapter(mAdapter);
+        activateFilter();
     }
 
 
@@ -72,7 +160,7 @@ public class MainActivity extends SystemBase implements RecyclerViewClickListene
     @Override
     public void onClick(View view, int position) {
         Intent intent = new Intent(this, DetailScreen.class);
-        intent.putExtra("pos",position);
+        intent.putExtra("pos", position);
         startActivity(intent);
     }
 }
